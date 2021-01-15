eac2ee89664e395fd3206e359ff3c74c32178572
VENTURA GUILLAUME p1908408 <guillaume.ventura@etu.univ-lyon1.fr>
2021-01-15 08:31:53 +0000

    [Create] Changement du bouton "Valider" en "Ajouter" et changement d'un lien mort

--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -27,12 +27,6 @@ class PostType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => Post::class,
-            'csrf_protection' => true,
-            // the name of the hidden HTML field that stores the token
-            'csrf_field_name' => '_token',
-            // an arbitrary string used to generate the value of the token
-            // using a different string for each form improves its security
-            'csrf_token_id'   => 'post',
         ]);
     }
 }
